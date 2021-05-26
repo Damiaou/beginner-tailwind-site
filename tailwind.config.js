ca52ce447b16dbd9636423402a85332c74469036
Damiaou <damsauvagere@gmail.com>
2021-05-26 20:11:29 +0200

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const { blue } = require('tailwindcss/colors');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+	purge: ['./*.html'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				orange: colors.orange,
+				teal: colors.teal,
+        'light-blue': colors.lightBlue
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
